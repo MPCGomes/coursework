9c1fa15d33a2297825a80d076a0a9e6415b8de62
MPCGomes <mpcgomes2@gmail.com>
2025-04-15 20:59:52 -0300

    add bradesco java cloud native bank account

---
+++ bradesco-java-cloud-native/bank-account/src/BankAccount.java
@@ -0,0 +1,42 @@
+public class BankAccount {
+
+    private int accountNumber;
+    private String agencyNumber;
+    private String clientName;
+    private double balance;
+
+    public BankAccount() {
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAgencyNumber() {
+        return this.agencyNumber;
+    }
+
+    public void setAgencyNumber(String agencyNumber) {
+        this.agencyNumber = agencyNumber;
+    }
+
+    public String getClientName() {
+        return this.clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
---
+++ bradesco-java-cloud-native/bank-account/src/InputUtil.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class InputUtil {
+
+    public static final Scanner scanner = new Scanner(System.in);
+}
