278a8c1b6c83ceb908f469678087f30a3c8be1d6
MPCGomes <150841644+MPCGomes@users.noreply.github.com>
2025-04-29 17:01:07 -0300

    add contact list application methods

--- college/contact-list/src/college/contactlist/ContactListApplication.java
+++ college/contact-list/src/college/contactlist/ContactListApplication.java
@@ -2,6 +2,8 @@ package college.contactlist;
 
 public class ContactListApplication {
     public static void main(String[] args) {
+        ContactList contactList = new ContactList();
 
+        contactList.showMenu();
     }
 }
